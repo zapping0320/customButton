318d34c8604de817ed7da845cbf7b0be42ef4da0
John DongHyun Kim <zapping0320@gmail.com>
2019-06-09 08:17:56 +0900

    Initial commit

---
+++ customButton/LoadingButton.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingButton.swift
+//  customButton
+//
+//  Created by 김동현 on 09/06/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class LoadingButton: UIButton {
+
+    let inidicator = UIActivityIndicatorView()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //init
+    }
+}
---
+++ customButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customButton
+//
+//  Created by 김동현 on 09/06/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
