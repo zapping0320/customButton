1a0b1c4062facd71892d0aa516a91401596e9d48
john donghyun kim <zapping0320@gmail.com>
2020-12-28 17:22:27 +0900

    UITextField를 custom으로 활용해봄
    - inputView, inputAccessoryView를 이용해서 textfield로 표시하지만 입력은 datepicker로 추가하도록 함

--- customButton/ViewController.swift
+++ customButton/ViewController.swift
@@ -14,12 +14,18 @@ class ViewController: UIViewController {
     
     var isLoading = false
     
+    let pickerView = UIDatePicker()
+    
+    @IBOutlet weak var basicTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         myButton.layer.borderColor = UIColor.black.cgColor
         myButton.layer.borderWidth = 1
+        
+        setDefaultTextField()
     }
 
     @IBAction func doAction(_ sender: LoadingButton) {
@@ -33,6 +39,58 @@ class ViewController: UIViewController {
             self.myButton.stopLoading()
         }
         
+    }
+    
+    func  setDefaultTextField()  {
+        self.basicTextField.clearButtonMode = .never
+        
+        guard let clearBtn = self.basicTextField.value(forKey: "clearButton") as? UIButton else {
+            return
+        }
+        clearBtn.setImage(#imageLiteral(resourceName: "downButton"), for: .normal)
+        
+        setPicker()
+        
+    }
+    
+    public func setPicker() {
+        pickerView.datePickerMode = .date
+        pickerView.locale = Locale(identifier: "ko_kr")
+        pickerView.timeZone = TimeZone(identifier: "KST")
+        pickerView.date = Date()
+        self.basicTextField.inputView = pickerView
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = .black
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem.init(title: "확인", style: .done, target: self, action: #selector(donePicker(_:)))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem.init(title: "취소", style: .done, target: self, action: #selector(cancelPicker(_:)))
+        
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        self.basicTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc func donePicker(_ sender: UIBarButtonItem) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = dateFormatter.string(from: self.pickerView.date)
+        print("selected -\(dateString)")
+        hide()
+    }
+    
+    @objc func cancelPicker(_ sender: UIBarButtonItem) {
+        print("cancel")
+        hide()
+    }
+    
+    public func hide() {
+        //_ = self.resignFirstResponder()
+        self.basicTextField.resignFirstResponder()
     }
     
 }
